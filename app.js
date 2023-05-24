f1424404ff68688861faf369ef9eb6aadd9b8c9d
tuan-hda <anhtuan.hoangdinh@gmail.com>
2023-05-24 22:44:31 +0700

    fix freaking typo swagger.yarml

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('trust proxy', 1)
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   })
 )
 app.use(express.json())
@@ -40,7 +40,7 @@ app.use(xss())
 // Swagger
 const swaggerUI = require('swagger-ui-express')
 const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./swagger.yarml')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 // routes
 app.get('/', (req, res) => {
@@ -59,9 +59,7 @@ const port = process.env.PORT || 5000
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    )
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`))
   } catch (error) {
     console.log(error)
   }
