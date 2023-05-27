f11aafecc19fef7a20b433b76f8a9ad3ece8750f
minhduc <minhductxqt@gmail.com>
2023-05-27 16:45:25 +0700

    remove rate limiter

--- app.js
+++ app.js
@@ -33,12 +33,12 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.set('trust proxy', 1)
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  })
-)
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100 // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//   })
+// )
 app.use(express.json())
 app.use(helmet())
 app.use(cors())
